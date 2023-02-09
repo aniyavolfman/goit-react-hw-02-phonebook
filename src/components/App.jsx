0a60ba28a7b6f338b3a120696af9715699c464cd
Anna Volfman <aniya_07@ukr.net>
2023-02-09 22:06:07 +0200

    add filter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,29 @@ import { nanoid } from 'nanoid';
 import { Container } from './Container/Container';
 import { Form } from './Form/Form';
 import { ContactsList } from './ContactsList/ContactsList';
+import { Filter } from './Filter/Filter';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
   };
 
   addContact = contact => {
+    if (
+      this.state.contacts.some(
+        contactItem =>
+          contactItem.name.toLowerCase() === contact.name.toLowerCase()
+      )
+    ) {
+      alert(`Oops, ${contact.name} is already in contacts!`);
+      return;
+    }
     const finalContact = {
       id: nanoid(),
       ...contact,
@@ -21,13 +36,24 @@ export class App extends Component {
     });
   };
 
+  handleFilter = ({ target: { value } }) => {
+    this.setState({
+      filter: value,
+    });
+  };
+
   render() {
+    const filteredContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().trim().includes(this.state.filter.toLowerCase())
+    );
+
     return (
       <Container>
         <h1>Phonebook</h1>
         <Form onSubmit={this.addContact} />
         <h2>Contacts</h2>
-        <ContactsList contacts={this.state.contacts} />
+        <Filter value={this.state.filter} onFilterChange={this.handleFilter} />
+        <ContactsList contacts={filteredContacts} />
       </Container>
     );
   }
